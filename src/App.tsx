f54903c47875328953f44ebd62f0ad79a8734e70
yeosg1 <yeosg1@gmail.com>
2023-09-14 22:10:58 +0900

    CSS 화면  이메일 계정생성의 이름 및 비밀번호 입력 페이지 작성 중

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Authentication from './views/Authentication';
+
+function App() {
+  return (
+    <div ><Authentication/></div>
+  );
+}
+
+export default App;
---
+++ src/views/Authentication/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './style.css';
+export default function Authentication() {
+  return (
+    <div id='auth-wrapper' >
+      
+      <div className='auth-card-container'>
+        <div className='auth-card-title-text'>{'갓생 살기'}</div>
+        <div className="auth-card-text-email">skin_lotion@naver.com</div>
+         
+         <div className='auth-card-input-container'>dsafdfs
+          <div className="auth-card-text-email-address">이메일 주소</div>
+          <div className="auth-card-text-email-input-box">saasdSAD
+            
+          </div>
+         </div>
+         
+        <div className="auth-card-email-box">
+        </div>
+      </div>
+    </div>
+  )
+}
