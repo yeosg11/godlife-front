15c4b1fc3217eab2ee75a3c428054d19be871653
yeosg1 <yeosg1@gmail.com>
2023-09-14 23:52:54 +0900

    2023-09-14 11:52

---
+++ src/types/board.interface.ts
@@ -0,0 +1,10 @@
+export default interface Board {
+    boardNumber: number;
+    title: string;
+    contents: string;
+    imageUrls: string[];
+    writeDatetime: string;
+    writerEmail: string;
+    nickname: string;
+    profileImageUrl: string | null;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import User from './user.interface';
+import Board from './board.interface';
+import LoginUser from './login-user.interface';
+import BoardItem from './board-item.interface';
+import CommentItem from './comment-item.interface';
+import FavoriteItem from './favorite-item.interface';
+
+export type { 
+    User,
+    Board,
+    LoginUser,
+    BoardItem,
+    CommentItem,
+    FavoriteItem,
+ };
